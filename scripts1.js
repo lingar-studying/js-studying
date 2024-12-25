9416ee6c13cb9a19f627604aee2ec4d5bf22fd74
Your Name <you@example.com>
2024-12-25 18:50:51 +0200

    check

--- scripts1.js
+++ scripts1.js
@@ -1,7 +1,7 @@
 console.log("Scripts 1");
 const input = document.getElementById("inputColorChange");
 const input1 = document.getElementById("inputSizeChange");
-
+let data = null;
 const square = document.getElementById("square1");
 
 function changeColor(){
@@ -17,4 +17,28 @@ function changeSize(){
     square.style.width = value +"px";
     square.style.height = value +"px";
 
+}
+
+function getProducts(){
+    console.log("getProducts");
+    
+    return fetch('https://dummyjson.com/products')
+    .then((res)=>{
+        
+       // data = res.json();
+        return res.json();
+    });
+    // .then(function(res) res.json())
+    // .then(res => res.json())
+    // .then(console.log);
+}
+function initPage2(){
+    getProducts().then(res=>{
+        console.log("got those products: ", res);
+        data = res;
+        
+    }).catch(er=>{
+        console.log("error happened ", er);
+        
+    })
 }
