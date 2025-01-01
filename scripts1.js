a3f5f30b32ac4321716b2a832d59895daebeec00
Your Name <you@example.com>
2025-01-01 19:13:02 +0200

    finishing js examples

--- scripts1.js
+++ scripts1.js
@@ -4,65 +4,132 @@ const input1 = document.getElementById("inputSizeChange");
 let data = null;
 const square = document.getElementById("square1");
 
-
+let filterData = [];
 const itemsGrid = document.getElementById("items-grid");
 
-function changeColor(){
+
+
+
+function changeColor() {
     console.log("change color");
 
     const value = input.value;
     console.log("value");
-    square.style.backgroundColor = value;   
-    
+    square.style.backgroundColor = value;
+
 }
-function changeSize(){
+
+function changeSize() {
     const value = input1.value;
-    square.style.width = value +"px";
-    square.style.height = value +"px";
+    square.style.width = value + "px";
+    square.style.height = value + "px";
 
 }
 
-function getProducts(){
+function getProducts() {
     console.log("getProducts");
-    
+
     return fetch('https://dummyjson.com/products')
-    .then((res)=>{
-        
-       // data = res.json();
-        return res.json();
-    });
+        .then((res) => {
+
+            // data = res.json();
+            return res.json();
+        });
     // .then(function(res) res.json())
     // .then(res => res.json())
     // .then(console.log);
 }
-function initPage2(){
-    getProducts().then(res=>{
+
+function initPage2() {
+    getProducts().then(res => {
         console.log("got those products: ", res);
         data = res;
         fillItems();
-        fillwithmap();
+        fillWithmap();
+        let x = 5 + 6 *7;
+        console.log("x = " + x);
+        filterData = data.products.map(item=>`${item.title} = ${item.price}`);
 
+
+        //add listner by input 
+
+        console.log("input = ", document.getElementById("filter-input"));
         
-    }).catch(er=>{
+         document.getElementById("filter-input").addEventListener("keyup", ()=>{
+
+            console.log("hi add listner");
+            
+            const inputFilter = document.getElementById("filter-input");
+            const value = inputFilter.value;
+            let finalData = [];
+            if(value.length>1){
+                finalData = filterData.filter(item=> {
+                    
+                    // console.log("item = ", item);
+                    
+                   return item.toLowerCase().includes(value.toLowerCase())});
+            }else{
+                finalData = filterData;
+            }
+            console.log("final-data = ", finalData);
+            
+            //show items 
+            const filteredArea = document.getElementById("filtered-data");
+            filteredArea.innerHTML = '';
+            finalData.forEach( item=> {
+
+                const newRow = document.createElement("p");
+                newRow.innerHTML = item;    
+                filteredArea.appendChild(newRow);
+            })
+
+        });
+
+        //making object of props as id only of expensive products
+        const reduced = data.products.reduce(outerFunction, {});
+
+        function outerFunction(obj, currentItem){
+            if(currentItem.price> 10){
+                obj["item" + currentItem.id] = currentItem;
+            }
+            return obj;
+        }
+
+        console.log("reduced - only expensive items, by id: \n", reduced );
+        
+
+    }).catch(er => {
         console.log("error happened ", er);
-        
+
     })
 }
 
-function fillwithmap(){
+function fillWithmap() {
     const simpleList = document.getElementById("simple-list");
-    const str = data.products.map(item =>{
 
-        return `<li>${item.title}, ${item.price}, ${item.description.substring(10)}...</li>`;
-        })
+    const simpleItemsData = data.products.map(item => {
 
-        simpleList.appendChild(str);
+        return `${item.title}, ${item.price}, ${item.description.substring(10)}...`;
         
-}
-function fillItems(){
-     
+    });
+    let string = "<h1>hhhh</h1>";
+    //simpleList.innerHTML = "<h1>hsdhsd</h1>"; //it will work
+    //simpleList.appendChild(string);
 
-    for(let i = 0; i < data.products.length; i++){
+    simpleItemsData.forEach(element => {
+        // console.log("element = " + element);
+        
+        // simpleList.appendChild(`<li>${element}</li>`)
+        simpleList.innerHTML += `<li>${element}</li>`;
+
+    });
+
+}
+
+function fillItems() {
+
+
+    for (let i = 0; i < data.products.length; i++) {
 
 
         const itemData = data.products[i];
